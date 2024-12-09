c88018689b9241c9f3e381302a0cc9bdbb76b953
Dang <56210432+looks32@users.noreply.github.com>
2024-12-10 00:12:37 +0900

    11 - axios 모듈 & vite 환경 변수 설정

--- src/router/index.js
+++ src/router/index.js
@@ -37,8 +37,8 @@ const routes = [
 	  path: '/posts/:id',
 	  name : 'PostDetail',
 	  component: PostDetailView ,
-	//   props : true,
-	  props : route => ({id : parseInt(route.params.id)})
+	  props : true
+	//   props : route => ({id : parseInt(route.params.id)})
 	},
 	{
 	  path: '/posts/:id/edit',
