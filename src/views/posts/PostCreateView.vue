3dced385f5b95476f004d9f2e689c81e51e13264
Dang <56210432+looks32@users.noreply.github.com>
2024-12-09 16:58:09 +0900

    8 - json-sever & axios
    문제 1
    게시글 등록 후 /posts로 이동했을 때 바로 등록된 게시글이 보여야하는데 새로고침을 해야만 보여짐
    
    문제 2
    글쓰기시 새로 만들어지는 데이터의 id가 숫자가 아닌 이상한 것들이 추가가 됨 숫자로만 순서대로 수정되도록 수정해야함

--- src/views/posts/PostCreateView.vue
+++ src/views/posts/PostCreateView.vue
@@ -2,14 +2,14 @@
 	<div>
 		<h2>게시글 등록</h2>
 		<hr class="my-4"/>
-		<form @submit.prevent>
+		<form @submit.prevent="save">
 			<div class="mb-3">
 				<label for="title" class="form-label">제목</label>
-				<input type="text" class="form-control" id="title"/>
+				<input v-model="form.title" type="text" class="form-control" id="title"/>
 			</div>
 			<div class="mb-3">
 				<label for="content" class="form-label">내용</label>
-				<textarea class="form-control" id="content" rows="3"></textarea>
+				<textarea v-model="form.content" class="form-control" id="content" rows="3"></textarea>
 			</div>
 			<div class="pt4">
 				<button type="button" class="btn btn-outline-dark me-2" @click="goListPage">목록</button>
@@ -20,10 +20,28 @@
 </template>
 
 <script setup>
+import { createPost } from '@/api/posts';
+import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 
 const router = useRouter();
+const form = ref({
+	title:null,
+	content:null
+})
+
+const save = () => {
+	try{
+		createPost({
+			...form.value,
+			createdAt : Date.now(),
+		});
+		router.push({name : 'PostList'})
+	} catch (error) {
+		console.error(error)
+	}
+}
 
 const goListPage = () => {
 	router.push({
